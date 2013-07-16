5e97402608d4e404f5b4f4ecdf710a225b41cb63
i09158knct <i09158knct@gmail.com>
2013-07-17 02:54:34 +0900

    add listener implementation

--- src/jp/knct/di/c6t/ui/exploration/ExplorationHomeActivity.java
+++ src/jp/knct/di/c6t/ui/exploration/ExplorationHomeActivity.java
@@ -1,14 +1,42 @@
 package jp.knct.di.c6t.ui.exploration;
 
 import jp.knct.di.c6t.R;
+import jp.knct.di.c6t.util.ActivityUtil;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class ExplorationHomeActivity extends Activity {
+public class ExplorationHomeActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_exploration_home);
 
+		ActivityUtil.setOnClickListener(this, this, new int[] {
+				R.id.exploration_home_create_new_exploration,
+				R.id.exploration_home_join_exploration,
+		});
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.exploration_home_create_new_exploration:
+			// TODO
+			// Intent intent = new Intent(this, .class);
+			// startActivity(intent);
+			break;
+
+		case R.id.exploration_home_join_exploration:
+			// TODO
+			// Intent intent = new Intent(this, .class);
+			// startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
 	}
 }
