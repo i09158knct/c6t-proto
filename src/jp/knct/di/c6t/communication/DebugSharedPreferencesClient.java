0a15fa4429cfab10b5b3a9b059314efcdc9cf76b
i09158knct <i09158knct@gmail.com>
2013-07-17 02:44:27 +0900

    fix bug for MyRoutesActivity

--- src/jp/knct/di/c6t/communication/DebugSharedPreferencesClient.java
+++ src/jp/knct/di/c6t/communication/DebugSharedPreferencesClient.java
@@ -1,5 +1,6 @@
 package jp.knct.di.c6t.communication;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import jp.knct.di.c6t.model.Route;
@@ -10,7 +11,6 @@ public class DebugSharedPreferencesClient implements Client {
 	@Override
 	public List<Route> getRoutes(User user) {
 		// TODO Auto-generated method stub
-		return null;
+		return new LinkedList<Route>();
 	}
-
 }
