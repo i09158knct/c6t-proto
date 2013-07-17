398356f8d802a934364bbb9b355d7d3727bad18f
i09158knct <i09158knct@gmail.com>
2013-07-18 00:13:56 +0900

    add save function

--- src/jp/knct/di/c6t/ui/exploration/NewExplorationActivity.java
+++ src/jp/knct/di/c6t/ui/exploration/NewExplorationActivity.java
@@ -1,17 +1,28 @@
 package jp.knct.di.c6t.ui.exploration;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import jp.knct.di.c6t.R;
+import jp.knct.di.c6t.communication.DebugSharedPreferencesClient;
+import jp.knct.di.c6t.model.Exploration;
+import jp.knct.di.c6t.model.Route;
+import jp.knct.di.c6t.model.User;
 import jp.knct.di.c6t.ui.route.IntentData;
 import jp.knct.di.c6t.util.ActivityUtil;
+
+import org.json.JSONException;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class NewExplorationActivity extends Activity implements OnClickListener {
-	private Parcelable mRoute;
+	private Route mRoute;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +32,49 @@ public class NewExplorationActivity extends Activity implements OnClickListener 
 		mRoute = getIntent().getParcelableExtra(IntentData.EXTRA_KEY_ROUTE);
 
 		ActivityUtil.setOnClickListener(this, this, new int[] {
-				R.id.route_create_new_exploration,
+				R.id.new_exploration_ok,
 		});
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.route_create_new_exploration:
-			Intent intent = new Intent(this, NewExplorationActivity.class)
-					.putExtra(IntentData.EXTRA_KEY_ROUTE, mRoute);
-			startActivity(intent);
+		case R.id.new_exploration_ok:
+			Exploration exploration = createExplorationFromForms();
+			new DebugSharedPreferencesClient(this).saveExploration(exploration);
+			try {
+				Toast.makeText(this, exploration.toJSON().toString(2), 1);
+			}
+			catch (JSONException e) {}
+
+			// TODO
+			// Intent intent = new Intent()
+			// .putExtra(IntentData.EXTRA_KEY_ROUTE, mRoute);
+			// startActivity(intent);
 			break;
 
 		default:
 			break;
 		}
+	}
+
+	private Exploration createExplorationFromForms() {
+		User myself = new DebugSharedPreferencesClient(this).getMyUserData();
+		Date date = getDateTime();
+		String description = ActivityUtil.getText(this, R.id.new_exploration_description);
+		return new Exploration(myself, mRoute, date, description);
+	}
+
+	private Date getDateTime() {
+		DatePicker datePicker = (DatePicker) findViewById(R.id.new_exploration_date);
+		TimePicker timePicker = (TimePicker) findViewById(R.id.new_exploration_time);
+
+		int year = datePicker.getYear();
+		int month = datePicker.getMonth();
+		int day = datePicker.getDayOfMonth();
+		int hour = timePicker.getCurrentHour();
+		int minute = timePicker.getCurrentMinute();
+		return new GregorianCalendar(year, month, day, hour, minute).getTime();
 	}
 
 }
