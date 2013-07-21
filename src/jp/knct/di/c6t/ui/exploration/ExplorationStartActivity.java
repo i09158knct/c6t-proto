fe98d7a05d966d2ec20ea25e19d26fcf4da6331d
i09158knct <i09158knct@gmail.com>
2013-07-22 03:53:28 +0900

    add initialize code of camera position

--- src/jp/knct/di/c6t/ui/exploration/ExplorationStartActivity.java
+++ src/jp/knct/di/c6t/ui/exploration/ExplorationStartActivity.java
@@ -3,42 +3,59 @@ package jp.knct.di.c6t.ui.exploration;
 import jp.knct.di.c6t.IntentData;
 import jp.knct.di.c6t.R;
 import jp.knct.di.c6t.model.Exploration;
-import jp.knct.di.c6t.model.Quest;
 import jp.knct.di.c6t.model.Route;
-import jp.knct.di.c6t.ui.route.RouteCreationDetailFormActivity;
-import jp.knct.di.c6t.ui.route.RouteCreationQuestFormActivity;
 import jp.knct.di.c6t.util.ActivityUtil;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
+import jp.knct.di.c6t.util.MapUtil;
 import android.app.Activity;
-import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class ExplorationStartActivity extends Activity implements OnClickListener {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ExplorationStartActivity extends Activity
+		implements OnClickListener,
+		ConnectionCallbacks,
+		OnConnectionFailedListener {
 	private GoogleMap mMap;
 	private Exploration mExploration;
+	private LocationClient mLocationClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_exploration_start);
-		
-		mExploration = getIntent().getParcelableExtra(IntentData.EXTRA_KEY_EXPLORATION);
-		
-		ActivityUtil.setOnClickListener(this, this, new int[] {
-		});
 
+		mExploration = getIntent().getParcelableExtra(IntentData.EXTRA_KEY_EXPLORATION);
+
+		ActivityUtil.setOnClickListener(this, this, new int[] {});
+
+		setUpMap();
+
+		mLocationClient = new LocationClient(this, this, this);
+		mLocationClient.connect();
+
+	}
+
+	private void setUpMap() {
 		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.exploration_start_map))
 				.getMap();
 		mMap.setMyLocationEnabled(true);
+
+		mMap.moveCamera(MapUtil.INITIAL_CAMERA_UPDATE);
 
 		MarkerOptions startPoint = createStartPointMarker(mExploration.getRoute());
 		mMap.addMarker(startPoint);
@@ -51,8 +68,43 @@ public class ExplorationStartActivity extends Activity implements OnClickListene
 				.title("スタートポイント");
 	}
 
+	private LatLng getCurrentLocation() {
+		Location lastLocation = mLocationClient.getLastLocation();
+		double latitude = lastLocation.getLatitude();
+		double longitude = lastLocation.getLongitude();
+		return new LatLng(latitude, longitude);
+	}
+
 	@Override
 	public void onClick(View v) {
 	}
 
+	@Override
+	public void onConnected(Bundle bundle) {
+		LatLngBounds bounds = new LatLngBounds.Builder()
+				.include(getCurrentLocation())
+				.include(mExploration.getRoute().getStartLocation())
+				.build();
+		final CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, 200);
+
+		// Cannot zoom to bounds until the map has a size.
+		// http://stackoverflow.com/questions/13692579/movecamera-with-cameraupdatefactory-newlatlngbounds-crashes
+		mMap.setOnCameraChangeListener(new OnCameraChangeListener() {
+			@Override
+			public void onCameraChange(CameraPosition arg0) {
+				mMap.animateCamera(update);
+				mMap.setOnCameraChangeListener(null);
+			}
+		});
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// TODO Auto-generated method stub
+	}
 }
