b80e76dfef118372134f8e15a9e70544861c3087
i09158knct <i09158knct@gmail.com>
2013-07-31 17:23:20 +0900

    extract ActivityLink class to ActivityUtil

--- src/jp/knct/di/c6t/ui/HomeActivity.java
+++ src/jp/knct/di/c6t/ui/HomeActivity.java
@@ -4,30 +4,19 @@ import jp.knct.di.c6t.R;
 import jp.knct.di.c6t.ui.exploration.ExplorationHomeActivity;
 import jp.knct.di.c6t.ui.route.MyRoutesActivity;
 import jp.knct.di.c6t.ui.route.RouteCreationActivity;
+import jp.knct.di.c6t.util.ActivityUtil;
+import jp.knct.di.c6t.util.ActivityUtil.ActivityLink;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 
 public class HomeActivity extends Activity {
-
-	private static class ActivityLink {
-		private final int id;
-		private final Class<? extends Activity> target;
-
-		public ActivityLink(int id, Class<? extends Activity> target) {
-			this.id = id;
-			this.target = target;
-		}
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 
-		setJumper(new ActivityLink[] {
+		ActivityUtil.setJumper(this, new ActivityLink[] {
 				new ActivityLink(R.id.home_collection, null), // TODO
 				new ActivityLink(R.id.home_ranking, null), // TODO
 				new ActivityLink(R.id.home_schedule, null), // TODO
@@ -37,15 +26,4 @@ public class HomeActivity extends Activity {
 		});
 	}
 
-	private void setJumper(ActivityLink... activityLinks) {
-		for (final ActivityLink activityLink : activityLinks) {
-			findViewById(activityLink.id).setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(HomeActivity.this, activityLink.target);
-					startActivity(intent);
-				}
-			});
-		}
-	}
 }
--- src/jp/knct/di/c6t/util/ActivityUtil.java
+++ src/jp/knct/di/c6t/util/ActivityUtil.java
@@ -1,10 +1,35 @@
 package jp.knct.di.c6t.util;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class ActivityUtil {
+
+	public static class ActivityLink {
+		public final int id;
+		public final Class<? extends Activity> target;
+
+		public ActivityLink(int id, Class<? extends Activity> target) {
+			this.id = id;
+			this.target = target;
+		}
+	}
+
+	public static void setJumper(final Activity activity, ActivityLink... activityLinks) {
+		for (final ActivityLink activityLink : activityLinks) {
+			activity.findViewById(activityLink.id).setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(activity, activityLink.target);
+					activity.startActivity(intent);
+				}
+			});
+		}
+	}
+
 	public static void setOnClickListener(Activity container, OnClickListener listener, int... ids) {
 		for (int id : ids) {
 			container.findViewById(id).setOnClickListener(listener);
