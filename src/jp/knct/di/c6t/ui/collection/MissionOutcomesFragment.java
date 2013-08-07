d9bb7e8112284829791c32dbf3b0bc575c00ac17
i09158knct <i09158knct@gmail.com>
2013-08-07 14:05:28 +0900

    fix to use Outcome models instead of raw models

--- src/jp/knct/di/c6t/ui/collection/MissionOutcomesFragment.java
+++ src/jp/knct/di/c6t/ui/collection/MissionOutcomesFragment.java
@@ -1,13 +1,16 @@
 package jp.knct.di.c6t.ui.collection;
 
-import java.util.LinkedList;
+import java.text.ParseException;
 import java.util.List;
 
 import jp.knct.di.c6t.R;
-import jp.knct.di.c6t.communication.DebugSharedPreferencesClient;
-import jp.knct.di.c6t.model.Quest;
-import jp.knct.di.c6t.model.Route;
+import jp.knct.di.c6t.communication.OutcomesClient;
+import jp.knct.di.c6t.model.MissionOutcome;
 import jp.knct.di.c6t.util.ActivityUtil;
+import jp.knct.di.c6t.util.TimeUtil;
+
+import org.json.JSONException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -15,47 +18,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 public class MissionOutcomesFragment extends ListFragment {
+	private List<MissionOutcome> mMissionOutcomes;
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		MissionsAdapter adapter = new MissionsAdapter(getActivity(), getMissions());
-		setListAdapter(adapter);
-	}
-
-	private List<Quest> getMissions() {
-		// TODO
-		DebugSharedPreferencesClient client = new DebugSharedPreferencesClient(getActivity());
-		List<Quest> quests = new LinkedList<Quest>();
-		for (Route route : client.getRoutes(client.getMyUserData())) {
-			quests.addAll(route.getQuests());
+		OutcomesClient client = new OutcomesClient();
+		try {
+			mMissionOutcomes = client.getMissionOutcomes(getActivity());
 		}
-		return quests;
+		catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		setListAdapter(new MissionOutcomesAdapter(getActivity(), mMissionOutcomes));
 	}
 
-	private class MissionsAdapter extends ArrayAdapter<Quest> {
-		private List<Quest> mMissions; // FIXME
+	private class MissionOutcomesAdapter extends ArrayAdapter<MissionOutcome> {
+		private List<MissionOutcome> mMissionOutcomes;
 		private LayoutInflater mInflater;
 
-		public MissionsAdapter(Activity context, List<Quest> missions) {
-			super(context, 0, missions);
-			mMissions = missions;
+		public MissionOutcomesAdapter(Activity context, List<MissionOutcome> missionOutcomes) {
+			super(context, 0, missionOutcomes);
+			mMissionOutcomes = missionOutcomes;
 			mInflater = context.getLayoutInflater();
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			Quest mission = mMissions.get(position);
-
 			if (convertView == null) {
 				convertView = mInflater.inflate(R.layout.list_item_outcome_mission, parent, false);
 			}
 
+			MissionOutcome missionOutcome = mMissionOutcomes.get(position);
 			new ActivityUtil(convertView)
-					.setText(R.id.list_item_outcome_mission_name, mission.getMission())
-					.setText(R.id.list_item_outcome_mission_photoed_at, "______"); // TODO
+					.setText(R.id.list_item_outcome_mission_name, missionOutcome.getMission())
+					.setText(R.id.list_item_outcome_mission_photoed_at, TimeUtil.format(missionOutcome.getPhotoedAt()));
+
+			((ImageView) convertView.findViewById(R.id.list_item_outcome_mission_photo))
+					.setImageBitmap(missionOutcome.decodePhotoBitmap(10));
 
 			return convertView;
 		}
