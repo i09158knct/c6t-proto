bcaef0383c9e850f99499b8b67ee521152eed094
i09158knct <i09158knct@gmail.com>
2013-09-05 00:16:47 +0900

    change to use a http client

--- src/jp/knct/di/c6t/ui/exploration/ExplorationHomeActivity.java
+++ src/jp/knct/di/c6t/ui/exploration/ExplorationHomeActivity.java
@@ -1,16 +1,22 @@
 package jp.knct.di.c6t.ui.exploration;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.List;
 
 import jp.knct.di.c6t.IntentData;
 import jp.knct.di.c6t.R;
-import jp.knct.di.c6t.communication.Client;
-import jp.knct.di.c6t.communication.DebugSharedPreferencesClient;
+import jp.knct.di.c6t.communication.BasicClient;
 import jp.knct.di.c6t.model.Exploration;
 import jp.knct.di.c6t.ui.route.SearchRouteActivity;
 import jp.knct.di.c6t.util.ActivityUtil;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,10 +35,7 @@ public class ExplorationHomeActivity extends ListActivity implements OnClickList
 				R.id.exploration_home_join_exploration,
 		});
 
-		Client client = new DebugSharedPreferencesClient(this);
-		mExplorations = client.getExplorations(client.getMyUserData());
-		ExplorationsAdapter adapter = new ExplorationsAdapter(this, mExplorations);
-		setListAdapter(adapter);
+		new LoadingTask().execute();
 	}
 
 	protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -59,6 +62,42 @@ public class ExplorationHomeActivity extends ListActivity implements OnClickList
 
 		default:
 			break;
+		}
+	}
+
+	private class LoadingTask extends AsyncTask<Void, Void, List<Exploration>> {
+
+		@Override
+		protected List<Exploration> doInBackground(Void... params) {
+			BasicClient client = new BasicClient();
+			List<Exploration> explorations = null;
+			try {
+				explorations = client.getExplorations(client.getUserFromLocal(getApplicationContext()));
+			}
+			catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return explorations;
+		}
+
+		@Override
+		protected void onPostExecute(List<Exploration> myExplorations) {
+			mExplorations = myExplorations;
+			ExplorationsAdapter adapter = new ExplorationsAdapter(ExplorationHomeActivity.this, mExplorations);
+			setListAdapter(adapter);
 		}
 	}
 }
